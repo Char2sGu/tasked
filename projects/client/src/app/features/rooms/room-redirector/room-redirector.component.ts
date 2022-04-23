676f12c3d9c6e87e4a2db0f460880ae5bd82c1da
Char2s <Char2s@outlook.com>
2022-04-23 13:28:58 +0800

    feat(client): remove notification on room navigation

--- projects/client/src/app/features/rooms/room-redirector/room-redirector.component.ts
+++ projects/client/src/app/features/rooms/room-redirector/room-redirector.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { NotifierService } from 'angular-notifier';
 import { combineLatest } from 'rxjs';
 import { first, map } from 'rxjs/operators';
 
-import { NotificationType } from '../../../common/notification-type.enum';
+import { skipFalsy } from '../../../common/rxjs.utils';
 import { RoomListGQL } from '../../../graphql';
 import { AuthService } from '../../auth/auth.service';
 import { RoomsActivatedMapStorage } from '../rooms-activated-map.storage';
@@ -19,26 +18,21 @@ export class RoomRedirectorComponent {
     private router: Router,
     private activatedRoomsMap: RoomsActivatedMapStorage,
     private auth: AuthService,
-    private notifier: NotifierService,
     private listGql: RoomListGQL,
   ) {}
 
   ngOnInit(): void {
     combineLatest([
       this.listGql.fetch().pipe(map((result) => result.data.rooms.results)),
-      this.auth.user$.pipe(first()),
+      this.auth.user$.pipe(first(), skipFalsy()),
     ]).subscribe(([rooms, user]) => {
       const map = this.activatedRoomsMap;
-      if (user!.id in map.value) {
-        const exists = rooms.some((item) => item.id == map.value[user!.id]);
+      if (user.id in map.value) {
+        const exists = rooms.some((item) => item.id == map.value[user.id]);
         if (exists) {
-          this.notifier.notify(
-            NotificationType.Info,
-            $localize`Navigated to the last accessed room`,
-          );
-          this.redirect(map.value[user!.id]);
+          this.redirect(map.value[user.id]);
         } else {
-          delete map.value[user!.id];
+          delete map.value[user.id];
           map.save();
           this.redirect();
         }
