07bbbf7324c014631594ff4db3bb433a0917aee7
Char2s <Char2s@outlook.com>
2022-04-24 16:15:46 +0800

    fix(client): fix fade through animation curves

--- projects/client/src/app/common/animations.ts
+++ projects/client/src/app/common/animations.ts
@@ -25,30 +25,24 @@ export class FadeThroughAnimation extends Animation {
   static override content: AnimationReferenceMetadata = animation([
     style({ position: 'relative' }),
     query(':enter, :leave', [
-      style({
-        position: 'absolute',
-        top: 0,
-        bottom: 0,
-        left: 0,
-        right: 0,
-      }),
+      style({ position: 'absolute', top: 0, bottom: 0, left: 0, right: 0 }),
     ]),
-    query(':leave', [style({ opacity: 1 })], { optional: true }),
-    query(':enter', [style({ transform: 'scale(92%)', opacity: 0 })]),
     group([
       query(
         ':leave',
         [
+          style({ opacity: 1 }),
           animate(
-            `90ms ${AnimationCurves.STANDARD_CURVE}`,
+            `90ms ${AnimationCurves.ACCELERATION_CURVE}`,
             style({ opacity: 0 }),
           ),
         ],
         { optional: true },
       ),
       query(':enter', [
+        style({ transform: 'scale(92%)', opacity: 0 }),
         animate(
-          `210ms 90ms ${AnimationCurves.STANDARD_CURVE}`,
+          `210ms 90ms ${AnimationCurves.DECELERATION_CURVE}`,
           style({ transform: 'scale(1)', opacity: 1 }),
         ),
       ]),
