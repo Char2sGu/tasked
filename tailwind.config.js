8f465d0094dda068e35cb63cb735b489c987568c
Char2s <Char2s@outlook.com>
2022-04-28 00:54:46 +0800

    refactor(client): enhance tailwind selector specificity

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   content: ['projects/**/*.{html,ts}'],
+  important: 'body.tailwind-enhance-specificity', // we use prefix `.theme-light` and `.theme-dark` to style components, so we need to make sure that the specificity is increased to override component styles
   theme: {
     extend: {},
   },
