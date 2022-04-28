3c263c785a787c0f1e26703da718e3833dbc9074
Char2s <Char2s@outlook.com>
2022-04-29 00:39:07 +0800

    fix(client): optimize illustration positions

--- projects/client/src/app/components/empty-state/empty-state-illustration/empty-state-illustration.component.ts
+++ projects/client/src/app/components/empty-state/empty-state-illustration/empty-state-illustration.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, HostBinding, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-empty-state-illustration',
@@ -6,6 +6,7 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./empty-state-illustration.component.scss'],
 })
 export class EmptyStateIllustrationComponent implements OnInit {
+  @HostBinding('class') class = 'block';
   @Input() headline?: string;
   @Input() message?: string;
 
