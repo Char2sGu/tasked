7feacf81c65283ed9da013c41fcaee598998d32e
Char2s <Char2s@outlook.com>
2022-04-28 22:59:41 +0800

    refactor(client): completely deprecate @angular/flex-layout

--- projects/client/src/app/components/modal/modal.directive.ts
+++ projects/client/src/app/components/modal/modal.directive.ts
@@ -1,10 +1,13 @@
+import { BreakpointObserver } from '@angular/cdk/layout';
 import { Directive, Injectable, TemplateRef } from '@angular/core';
-import { MediaObserver } from '@angular/flex-layout';
 import {
   MatBottomSheet,
   MatBottomSheetRef,
 } from '@angular/material/bottom-sheet';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { first, map } from 'rxjs';
+
+import { Breakpoint } from '../../common/breakpoint.enum';
 
 @Injectable()
 export class ModalRef {
@@ -22,18 +25,22 @@ export class ModalRef {
 export class ModalDirective {
   private sheetRef?: MatBottomSheetRef<never>;
   private dialogRef?: MatDialogRef<never>;
+  private isPhone$ = this.breakpointObserver
+    .observe(Breakpoint.Small)
+    .pipe(map((state) => !state.matches));
 
   constructor(
     private templateRef: TemplateRef<never>,
-    private media: MediaObserver,
+    private breakpointObserver: BreakpointObserver,
     private dialog: MatDialog,
     private sheet: MatBottomSheet,
   ) {}
 
-  open(breakpoint = 'xs'): void {
-    const isPhone = this.media.isActive(breakpoint);
-    if (isPhone) this.openSheet();
-    else this.openDialog();
+  open(): void {
+    this.isPhone$.pipe(first()).subscribe((isPhone) => {
+      if (isPhone) this.openSheet();
+      else this.openDialog();
+    });
   }
 
   openDialog(): void {
--- projects/client/src/app/shared/shared.module.ts
+++ projects/client/src/app/shared/shared.module.ts
@@ -1,6 +1,5 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
 
 import { ValidationModule } from '../validation/validation.module';
@@ -8,12 +7,6 @@ import { RouterEventsDirective } from './router-events.directive';
 
 @NgModule({
   declarations: [RouterEventsDirective],
-  exports: [
-    CommonModule,
-    FormsModule,
-    FlexLayoutModule,
-    ValidationModule,
-    RouterEventsDirective,
-  ],
+  exports: [CommonModule, FormsModule, ValidationModule, RouterEventsDirective],
 })
 export class SharedModule {}
